17afa13d3e4927f6fb738c8df431e5254419f8a9
zhangping12 <634001184@qq.com>
2021-09-12 10:25:40 +0800

    用户模块开发

---
+++ cloud-mall-user/src/main/java/com/imooc/cloud/mall/practice/user/service/UserService.java
@@ -0,0 +1,25 @@
+package com.imooc.cloud.mall.practice.user.service;
+
+
+import com.imooc.cloud.mall.practice.common.exception.ImoocMallException;
+import com.imooc.cloud.mall.practice.user.model.pojo.User;
+
+/**
+ * 描述 UserService
+ */
+public interface UserService {
+
+    /**
+     * 注册
+     *
+     * @param userName
+     * @param password
+     */
+    void register(String userName, String password) throws ImoocMallException;
+
+    User login(String userName, String password) throws ImoocMallException;
+
+    void updateInformation(User user) throws ImoocMallException;
+
+    boolean checkAdminRole(User user);
+}
---
+++ cloud-mall-user/src/main/java/com/imooc/cloud/mall/practice/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,72 @@
+package com.imooc.cloud.mall.practice.user.service.impl;
+
+import com.imooc.cloud.mall.practice.common.exception.ImoocMallException;
+import com.imooc.cloud.mall.practice.common.exception.ImoocMallExceptionEnum;
+import com.imooc.cloud.mall.practice.common.util.MD5Utils;
+import com.imooc.cloud.mall.practice.user.model.dao.UserMapper;
+import com.imooc.cloud.mall.practice.user.model.pojo.User;
+import com.imooc.cloud.mall.practice.user.service.UserService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 描述：  USerService实现类
+ */
+@Service
+public class UserServiceImpl implements UserService {
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public void register(String userName, String password) throws ImoocMallException {
+        //查询用户名是否存在，不允许重名
+        User result = userMapper.selectByName(userName);
+        if (result != null) {
+            throw new ImoocMallException(ImoocMallExceptionEnum.NAME_EXISTED);
+        }
+        //写到数据库
+        User user = new User();
+        user.setUsername(userName);
+        try {
+            user.setPassword(MD5Utils.getMD5Str(password));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        int count = userMapper.insertSelective(user);//存在的字段才插入
+        if (count == 0) {
+            throw new ImoocMallException(ImoocMallExceptionEnum.INSERT_FAILED);
+        }
+    }
+
+    @Override
+    public User login(String userName, String password) throws ImoocMallException {
+        String md5Password = null;
+        try {
+            md5Password = MD5Utils.getMD5Str(password);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        User user = userMapper.selectLogin(userName, md5Password);
+        if (user == null) {
+            throw new ImoocMallException(ImoocMallExceptionEnum.WRONG_PASSWORD);
+        }
+        return user;
+    }
+
+    @Override
+    public void updateInformation(User user) throws ImoocMallException {
+        //更新个性签名
+        int updateCount = userMapper.updateByPrimaryKeySelective(user);
+        if (updateCount > 1) {
+            throw new ImoocMallException(ImoocMallExceptionEnum.UPDATE_FAILED);
+        }
+    }
+
+    @Override
+    public boolean checkAdminRole(User user) {
+        //1是普通用户，2是管理员，3是测试用户
+        return user.getRole().equals(2);
+    }
+}
